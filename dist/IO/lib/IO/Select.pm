506750057a0889773084385459c1bab193f3f06b
James E Keenan <jkeenan@cpan.org>
2021-08-09 21:09:27 +0000

    IO::Select: add debugging code
    The purpose of this debugging code is to expose a case where a
    "Use of uninitialized value in numeric eq (==)" warning may be generated
    when the code is running under a full "use warnings".

--- dist/IO/lib/IO/Select.pm
+++ dist/IO/lib/IO/Select.pm
@@ -67,8 +67,15 @@ sub _fileno
   # Neither a plain file number nor an opened filehandle; but maybe it was
   # previously registered and has since been closed. ->remove still wants to
   # know what fileno it had
+  #use warnings;
+  print STDERR "AAA: range: first_fd: ", FIRST_FD, "\tmax index: $#$self\n";
+  print STDERR "AAA: \$f is ", (defined($f) ? "defined" : "not defined"), "\n";
   foreach my $i ( FIRST_FD .. $#$self ) {
-   return $i - FIRST_FD if $self->[$i] == $f;
+    print STDERR "BBB: \$self->[$i] is ",
+        (defined($self->[$i]) ? "defined as $self->[$i]" : "not defined"), "\n";
+   if ($self->[$i] == $f) {
+    return $i - FIRST_FD;
+   }
   }
   return undef;
  }
