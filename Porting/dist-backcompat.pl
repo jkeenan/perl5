50e2eeac2927b3c218344b49d21ad59e3cbd8d73
James E Keenan <jkeenan@cpan.org>
2022-01-08 20:46:29 +0000

    Update inline comments
    Correct search for 'unreleased'.
    
    Tabulate status categories once, at (current) end of program.

--- Porting/dist-backcompat.pl
+++ Porting/dist-backcompat.pl
@@ -1,22 +1,16 @@
 #!/usr/bin/env perl
 use 5.14.0;
 use warnings;
-#use Data::Dump ( qw| dd pp| );
-#use Carp;
 use Cwd;
-use Data::Dumper;$Data::Dumper::Indent=1;
 use File::Find;
 use File::Spec;
-
-#my $dir = $ENV{PERL_WORKDIR};
-#chdir $dir or croak "Unable to chdir to git checkout of Perl 5";
+#use Data::Dump qw( dd pp );
 
 # TODO: Check that we're in the top-level directory of the core distribution
 
 my $dir = cwd();
 my $maint_file = File::Spec->catfile($dir, 'Porting', 'Maintainers.pl');
-require $maint_file;
-#dd( { %Maintainers::Modules } );
+require $maint_file;   # to get %Modules in package Maintainers
 
 my $manilib_file = File::Spec->catfile($dir, 'Porting', 'manifest_lib.pl');
 require $manilib_file; # to get function sort_manifest()
@@ -27,25 +21,9 @@ for my $m (keys %Maintainers::Modules) {
         $distmodules{$m} = $Maintainers::Modules{$m};
     }
 }
-#dd( { %distmodules } );
-say "Located ", scalar keys %distmodules, " 'dist/' entries in \%Maintainers::Modules";
-
-my %makefile_pl_status = ();
-for my $m (keys %distmodules) {
-    if (! exists $distmodules{$m}{DISTRIBUTION}) {
-        $makefile_pl_status{$m} = 'unreleased';
-    }
-}
-#say "Are these modules unreleased to CPAN?";
-#say $_ for sort keys %unreleased;
-#say '';
-#dd(\%makefile_pl_status);
-my ($current_count, $last_count);
-$current_count = $last_count = scalar keys %makefile_pl_status;
 
 # Sanity checks; all modules under dist/ should be blead-upstream and have P5P
 # as maintainer.
-
 for my $m (keys %distmodules) {
     if ($distmodules{$m}{UPSTREAM} ne 'blead') {
         warn "Distro $m has UPSTREAM other than 'blead'";
@@ -54,6 +32,26 @@ for my $m (keys %distmodules) {
         warn "Distro $m has MAINTAINER other than 'P5P'";
     }
 }
+
+#pp(\%distmodules);
+say "Located ", scalar keys %distmodules, " 'dist/' entries in \%Maintainers::Modules";
+
+# Order of Battle:
+
+# First, identify any dist/ distros which appear not to have current releases
+# on CPAN.  We'll call these 'unreleased'.
+
+my %makefile_pl_status = ();
+
+for my $m (keys %distmodules) {
+    if (! exists $distmodules{$m}{DISTRIBUTION}) {
+        my ($distname) = $distmodules{$m}{FILES} =~ m{^dist/(.*)/?$};
+        $makefile_pl_status{$distname} = 'unreleased';
+    }
+}
+
+# Second, identify those dist/ distros which have their own hard-coded
+# Makefile.PLs in the core distribution.  We'll call these 'native'.
 
 my $manifest = File::Spec->catfile($dir, 'MANIFEST');
 my @sorted = read_manifest($manifest);
@@ -67,22 +65,14 @@ for my $f (@sorted) {
             unless exists $makefile_pl_status{$distro};
     }
 }
-#dd(\%makefile_pl_status);
-$current_count = scalar keys %makefile_pl_status;
-my $native_count = $current_count - $last_count;
-say "$native_count 'dist/' distros have native Makefile.PLs";
-$last_count = $current_count;
+
+# Third, identify those dist/ distros whose Makefile.PL is generated during
+# Perl's own 'make' process.
 
 find(\&get_generated_makefiles, ( "$dir/dist" ));
 
-#dd(\%makefile_pl_status);
-$current_count = scalar keys %makefile_pl_status;
-my $generated_count = $current_count - $last_count;
-say "$generated_count 'dist/' distros have Makefile.PLs generated during Perl 5 'make'";
-$last_count = $current_count;
-
-# I now need to complete population of %makefile_pl_status with KVPs for those
-# distros whose Makefile.PL must be obtained from CPAN.
+# Fourth, identify those dist/ distros whose Makefile.PLs must presumably be
+# obtained from CPAN.
 
 for my $d (sort keys %distmodules) {
     next unless exists $distmodules{$d}{FILES};
@@ -93,14 +83,19 @@ for my $d (sort keys %distmodules) {
         $makefile_pl_status{$distname} = 'cpan';
     }
 }
-#dd(\%makefile_pl_status);
-$current_count = scalar keys %makefile_pl_status;
-my $cpan_count = $current_count - $last_count;
-say "$cpan_count 'dist/' distros (presumably) get their Makefile.PLs from CPAN";
-$last_count = $current_count;
 
+#pp(\%makefile_pl_status);
+my %counts;
+for my $module (sort keys %makefile_pl_status) {
+    $counts{$makefile_pl_status{$module}}++;
+}
+for my $k (sort keys %counts) {
+    printf "%-20s%4s\n" => ($k, $counts{$k});
+}
 
-say "Finished!";
+say "\nFinished!";
+
+##### SUBROUTINES #####
 
 sub get_generated_makefiles {
     if ( $File::Find::name =~ m{^$dir/dist/(.*?)/Makefile\.PL$} ) {
