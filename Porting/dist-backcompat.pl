4aba2d7575a70f3d32c4910065535fb4659a7666
James E Keenan <jkeenan@cpan.org>
2022-01-09 21:42:51 +0000

    Create show_makefile_pl_status()
    This function merely wraps up code used within the program.  At this point
    none of the program's functions are intended to be exportable.
    
    Start to write documentation in POD format.

--- Porting/dist-backcompat.pl
+++ Porting/dist-backcompat.pl
@@ -19,6 +19,108 @@ use Data::Dump qw( dd pp );
 #   tested; default will be testing all which have a CPAN release (currently,
 #   40). BASIC FRAMEWORK DONE 01/08/2021; selections not yet used.
 
+=head1 NAME
+
+Porting/dist-backcompat.pl - Will changes to F<dist/> build on older C<perl>s?
+
+=head1 SYNOPSIS
+
+TK
+
+=head1 DESCRIPTION
+
+As of Jan 09 2022, there are 41 distributions ("distros") underneath F<dist/>
+in the Perl 5 core distribution.  By definition, all of these are maintained
+by Perl 5 Porters in core but are potentially releasable to CPAN so that they
+may be installed against older F<perl>s.  (As of that date, all but one of
+those 41 distros has had at least one CPAN release in the past.)
+
+But if were to release the code in a given F<dist/> distro to CPAN today,
+would it build and test correctly against older F<perl>s?  I<Which> older
+F<perl>s?  More to the point, will changes to the code in these distros in
+core that we've made since the last production release of F<perl> cause
+failures if and when these distros get their next CPAN release?
+
+This program, F<Porting/dist-backcompat.pl>, aims to be a P5P core development
+tool which, when run in advance of a development, production or maintenance
+release of F<perl>, can alert a release manager or core developer to potential
+problems as described above.
+
+=head2 Terminology
+
+Every one of the F<dist/> distros has its own history, quirks and coding
+challenges.  So within this program we will use certain terminology to group
+distros that share certain characteristics.
+
+=head3 CPAN Viability
+
+Setting aside metadata files like F<META.json>, F<META.yml> and F<Changes>, if
+we were to take the code for a given F<dist/> distro as it stands today, added
+a F<Makefile.PL> as needed (see next section), rolled it up into a tarball and
+uploaded that tarball to CPAN, how would that CPAN release fare on
+L<CPANtesters|https://www.cpantesters.org> against older versions of F<perl>?
+
+If such a release required a lot of fine-tuning in order to get C<PASS>es on
+CPANtesters, then we would say it has I<low> direct CPAN viability.
+
+If such a release required little fine-tuning to get those C<PASS>es, then we
+would say it has I<high> direct CPAN viability.
+
+=head3 F<Makefile.PL> Status
+
+When any of these F<dist/> distros gets a CPAN release, it needs to have a
+F<Makefile.PL> so that F<ExtUtils::MakeMaker> can generated a F<Makefile>.
+But that doesn't mean that a given F<dist/> distro has a F<Makefile.PL> of its
+own within the core distribution.  We can classify these distros according to
+the following statuses:
+
+=over 4
+
+=item * C<unreleased>
+
+This kind of F<dist/> distro has apparently never had a CPAN release, so it
+has never needed a F<Makefile.PL> for that purpose and doesn't have one in
+core.
+
+=item * C<native>
+
+This kind of F<dist/> distro has its own F<Makefile.PL> directly coded in its
+directory underneath F<dist/> in core.  Such a distro may -- I<or may not> --
+use that very same F<Makefile.PL> in its CPAN release.
+
+=item * C<generated>
+
+This kind of F<dist/> distro does not have its own F<Makefile.PL> directly
+coded in its directory underneath F<dist/> in core.  Instead, its
+F<Makefile.PL> is generated during the Perl 5 build process (I<e.g.,> by a
+program called within F<make> such as F<make_ext.pl>).  Such a distro may --
+I<or may not> -- use that F<Makefile.PL> in its CPAN release.
+
+=item * C<cpan>
+
+This kind of F<dist/> distro has no F<Makefile.PL> of its own in the core
+distribution -- neither C<native> nor C<generated>.  Hence, when released to
+CPAN, the CPAN maintainer has to provide an appropriate, directly coded
+F<Makefile.PL> as part of the tarball.
+
+=back
+
+As of the date of this progam, the 4 different Makefile.PL statuses have these
+counts:
+
+    unreleased           1
+    native              15
+    generated            4
+    cpan                21
+
+As a consequence of this variation, this program will have to jump through
+significant hoops to get a reasonable estimate of the CPAN viability of each
+F<dist/> distro.
+
+=cut
+
+##### CHECK ENVIRONMENT #####
+
 my $verbose = '';
 my @distros_requested = ();
 GetOptions(
@@ -40,7 +142,6 @@ if (@wanthyphens) {
 
 my $describe = `git describe`;
 chomp($describe);
-#|) and die "Unable to call 'git describe'";
 
 my $dir = cwd();
 my $maint_file = File::Spec->catfile($dir, 'Porting', 'Maintainers.pl');
@@ -67,7 +168,6 @@ for my $m (keys %distmodules) {
     }
 }
 
-#pp(\%distmodules);
 if ($verbose) {
     say "Porting/dist-backcompat.pl";
     my $ldescribe = length $describe;
@@ -128,22 +228,7 @@ for my $d (sort keys %distmodules) {
     }
 }
 
-#pp(\%makefile_pl_status);
-my %counts;
-for my $module (sort keys %makefile_pl_status) {
-    $counts{$makefile_pl_status{$module}}++;
-}
-if ($verbose) {
-    for my $k (sort keys %counts) {
-        printf "  %-18s%4s\n" => ($k, $counts{$k});
-    }
-    say '';
-    printf "%-32s%-12s\n" => ('Distribution', 'Status');
-    printf "%-32s%-12s\n" => ('------------', '------');
-    for my $module (sort keys %makefile_pl_status) {
-        printf "%-32s%-12s\n" => ($module, $makefile_pl_status{$module});
-    }
-}
+show_makefile_pl_status(\%makefile_pl_status, $verbose);
 
 my @distros_for_testing = (scalar @distros_requested)
     ? @distros_requested
@@ -254,6 +339,32 @@ say "\nFinished!" if $verbose;
 
 ##### SUBROUTINES #####
 
+=head1 SUBROUTINES
+
+=head2 C<get_generated_makefiles()>
+
+=over 4
+
+=item * Purpose
+
+Identify those distros under F<dist/> whose F<Makefile.PL>s are generated by the Perl 5 build process.
+
+=item * Arguments
+
+None, as this is a C<\&wanted>-style code reference for C<File::Find::find>.
+
+=item * Return Value
+
+None.
+
+=item * Comment
+
+Internally assigns to the hash holding the status of F<Makefile.PL>s.
+
+=back
+
+=cut
+
 sub get_generated_makefiles {
     if ( $File::Find::name =~ m{^$dir/dist/(.*?)/Makefile\.PL$} ) {
         my $distro = $1;
@@ -262,6 +373,34 @@ sub get_generated_makefiles {
         }
     }
 }
+
+=head2 C<read_manifest()>
+
+=over 4
+
+=item * Purpose
+
+Get a sorted list of all files in F<MANIFEST> (without their descriptions).
+
+=item * Arguments
+
+    read_manifest('/path/to/MANIFEST');
+
+One scalar: the path to F<MANIFEST> in a git checkout of the Perl 5 core distribution.
+
+=item * Return Value
+
+List (sorted) of all files in F<MANIFEST>.
+
+=item * Comments
+
+Depends on C<sort_manifest()> from F<Porting/manifest_lib.pl>.
+
+(This is so elementary and useful that it should probably be in F<Porting/manifest_lib.pl>!)
+
+=back
+
+=cut
 
 sub read_manifest {
     my $manifest = shift;
@@ -272,4 +411,45 @@ sub read_manifest {
 
     my %seen= ( '' => 1 ); # filter out blank lines
     return grep { !$seen{$_}++ } sort_manifest(@manifest);
+}
+
+=head2 C<show_makefile_pl_status>
+
+=over 4
+
+=item * Purpose
+
+Display a chart listing F<dist/> distros in one column and the status of their respective F<Makefile.PL>s in the second column.
+
+=item * Arguments
+
+    show_makefile_pl_status(\%makefile_pl_status, $verbose);
+
+List of two scalars:  (i) Reference to the hash holding status of F<Makefile.PL> for each F<dist/> distro; (ii) verbosity request.
+
+=item * Return Value
+
+Implicitly returns true on success, but does not return any otherwise meaningful value.
+
+=back
+
+=cut
+
+sub show_makefile_pl_status {
+    my ($status, $verbose) = @_;
+    my %counts;
+    for my $module (sort keys %{$status}) {
+        $counts{$status->{$module}}++;
+    }
+    if ($verbose) {
+        for my $k (sort keys %counts) {
+            printf "  %-18s%4s\n" => ($k, $counts{$k});
+        }
+        say '';
+        printf "%-32s%-12s\n" => ('Distribution', 'Status');
+        printf "%-32s%-12s\n" => ('------------', '------');
+        for my $module (sort keys %{$status}) {
+            printf "%-32s%-12s\n" => ($module, $status->{$module});
+        }
+    }
 }
