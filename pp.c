e35d986004f93fd52568dbd2829d298f3263dc68
David Mitchell <davem@iabyn.com>
2025-01-20 11:03:02 +0000

    fix refassign in non-void context
    GH #22710
    
    The experimental refassign feature, \@a = $aref, which allows an
    array/hash etc to be aliased rather than copied, got broken in rvalue
    non-void context by v5.39.5-56-g604c0355d2. So in something like
    
        my $x = (\@a = $aref);
    
    the stack was underflowing. This is because previously pp_refassign()
    would leave its RH argument on the stack when returning, while my commit
    above made it always pop the argument off the stack.
    
    This commit makes it only pop the arg in void context.
    
    Signed-off-by: James E Keenan <jkeenan@cpan.org>

--- pp.c
+++ pp.c
@@ -7439,7 +7439,7 @@ PP(pp_refassign)
            in leavesub?  */
     }
     else
-        rpp_popfree_to_NN(PL_stack_sp - (extra + 1));
+        rpp_popfree_to_NN(PL_stack_sp - (extra + cBOOL(GIMME_V == G_VOID)));
 
     return NORMAL;
 }
