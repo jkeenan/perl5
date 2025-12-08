0cacab84f4bcb70a9a0083981f7ec48b6aa2f2ec
James E Keenan <jkeenan@cpan.org>
2025-12-08 10:20:36 -0500

    Fatalization of calling import/unimport method with argument
    This commit is mostly a manual reversion of commit f1cf82e.  Return to a
    perl_croak in universal.c; adjust regen/warnings.pl as needed; run 'make
    regen'; get t/op/universal.t passing.  No documentation changes yet.
    
    Add test for undefined unimport method, then refactor repeated code into
    a subroutine.  Perform a pattern match rather than a string equality
    test because testing for exact line numbers inside a test program too
    fragile for maintenance purposes.
    
    'mispelled' was misspelled
    
    Still one porting test failure.
    
    For: GH #23623

--- warnings.h
+++ warnings.h
@@ -150,26 +150,22 @@
 
 #define WARN_EXPERIMENTAL__CLASS	 75
 
-/* Warnings Categories added in Perl 5.039002 */
-
-#define WARN_DEPRECATED__MISSING_IMPORT_CALLED_WITH_ARGS 76
-
 /* Warnings Categories added in Perl 5.039008 */
 
-#define WARN_DEPRECATED__SUBSEQUENT_USE_VERSION 77
+#define WARN_DEPRECATED__SUBSEQUENT_USE_VERSION 76
 
 /* Warnings Categories added in Perl 5.041 */
 
-#define WARN_EXPERIMENTAL__KEYWORD_ALL	 78
-#define WARN_EXPERIMENTAL__KEYWORD_ANY	 79
+#define WARN_EXPERIMENTAL__KEYWORD_ALL	 77
+#define WARN_EXPERIMENTAL__KEYWORD_ANY	 78
 
 /* Warnings Categories added in Perl 5.043 */
 
-#define WARN_EXPERIMENTAL__SIGNATURE_NAMED_PARAMETERS 80
-#define WARNsize			 21
-#define WARN_ALLstring			 "\125\125\125\125\125\125\125\125\125\125\125\125\125\125\125\125\125\125\125\125\125"
-#define WARN_NONEstring			 "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
-#define WARN_DEFAULTstring		 "\x10\x01\x00\x00\x00\x50\x04\x00\x00\x00\x00\x00\x05\x00\x15\x14\x55\x55\x54\x55\x01"
+#define WARN_EXPERIMENTAL__SIGNATURE_NAMED_PARAMETERS 79
+#define WARNsize			 20
+#define WARN_ALLstring			 "\125\125\125\125\125\125\125\125\125\125\125\125\125\125\125\125\125\125\125\125"
+#define WARN_NONEstring			 "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+#define WARN_DEFAULTstring		 "\x10\x01\x00\x00\x00\x50\x04\x00\x00\x00\x00\x00\x05\x00\x15\x14\x55\x55\x54\x55"
 
 #define isLEXWARN_on \
         cBOOL(PL_curcop && PL_curcop->cop_warnings != pWARN_STD)
@@ -357,7 +353,6 @@ category parameters passed.
 =for apidoc Amnh||WARN_DEPRECATED__VERSION_DOWNGRADE
 =for apidoc Amnh||WARN_DEPRECATED__DELIMITER_WILL_BE_PAIRED
 =for apidoc Amnh||WARN_EXPERIMENTAL__CLASS
-=for apidoc Amnh||WARN_DEPRECATED__MISSING_IMPORT_CALLED_WITH_ARGS
 =for apidoc Amnh||WARN_DEPRECATED__SUBSEQUENT_USE_VERSION
 =for apidoc Amnh||WARN_EXPERIMENTAL__KEYWORD_ALL
 =for apidoc Amnh||WARN_EXPERIMENTAL__KEYWORD_ANY
