2c334a81b11f2f4e7018be7b1c77e7a2cb6bb6cc
James E Keenan <jkeenan@cpan.org>
2025-12-17 19:43:30 -0500

    Eliminate uninitialized value warning
    We were getting: Use of uninitialized value $SelfLoader::AttrList in
    regexp compilation at ../../lib/SelfLoader.pm line 114

--- dist/SelfLoader/lib/SelfLoader.pm
+++ dist/SelfLoader/lib/SelfLoader.pm
@@ -6,7 +6,7 @@ our $VERSION = "1.28";
 
 # The following bit of eval-magic is necessary to make this work on
 # perls < 5.009005.
-our $AttrList;
+our $AttrList = '';
 BEGIN {
   if ($] > 5.009004) {
     eval <<'NEWERPERL';
