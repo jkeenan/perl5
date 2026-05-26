c6647eb9d8dd5f440877f5e617c8e10ee249b22a
James E Keenan <jkeenan@cpan.org>
2026-05-26 17:34:18 -0400

    cpan/Socket - Update to version 2.041
    2.041   2026-03-26
            [CHANGES]
             * Modernise Pod documentation formatting
    
            [BUGFIXES]
             * Fix reuse of `STRLEN len` variable in pack_ip_mreq_source()

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1040,7 +1040,8 @@ our %Modules = (
     },
 
     'Socket' => {
-        'DISTRIBUTION' => 'PEVANS/Socket-2.038.tar.gz',
+        'DISTRIBUTION' => 'PEVANS/Socket-2.041.tar.gz',
+        'SYNCINFO'     => 'jkeenan on Tue May 26 17:33:38 2026',
         'SYNCINFO'     => 'LeoNerd on Sat Apr 27 09:57:02 2024',
         'FILES'        => q[cpan/Socket],
         'EXCLUDED'     => ['.editorconfig'],
